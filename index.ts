636e580da66d03e15bc2abedbc51c9288a915da4
Vipul K Mishra <Vipul.Mishra@gdit.com>
2024-07-19 05:11:35 -0400

    Classes & Interfaces

--- index.ts
+++ index.ts
@@ -1,1 +1,64 @@
-const myName: string = 'Steve';
+const add = (a: number, b: number): number => {
+    
+     return a + b;
+ }
+
+//anonymous funciton
+
+ const joinString = function (a: string, b: string): string{
+    
+     return a + b;
+}
+
+//      --------------------------------------------------------------------------
+
+interface Post{
+
+    title: string;
+    dayold: number;
+    published: boolean;
+    
+}
+
+const post1 = {
+     title: 'aa',
+    dayold:33,
+    published:true
+    
+}
+
+const printPost = (p: Post) => {
+    
+    console.log(`${p.dayold} && ${p.published} `);
+}
+
+printPost(post1);
+
+//  classes and properties    --------------------------------------------------------------------------
+
+const Component = (target:any) => {
+    
+    console.log(target);
+
+}
+
+@Component
+class Car{
+
+    constructor(private color: string, private model: string) { }
+    
+    drive() {
+        console.log(this.color);
+    }
+}
+
+const car1 = new Car('red', 'ferrari');
+
+// ---------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
